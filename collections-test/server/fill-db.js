cee9dc06ed02274711e421a7d922c74b54c13bd9
uzumaxy <uzumaxy@gmail.com>
2016-07-31 23:50:41 +0300

    Moved fill-db script to server-side.

---
+++ collections-test/server/fill-db.js
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+import { ProgrammingLanguages } from '/collections';
+
+Meteor.startup(() => {
+    const count = ProgrammingLanguages.find().count();
+
+    if (count === 0) {
+      ProgrammingLanguages.insert({
+        name: 'JavaScript',
+        stars: 0
+      });
+      ProgrammingLanguages.insert({
+        name: 'Python',
+        stars: 0
+      });
+      ProgrammingLanguages.insert({
+        name: 'Ruby',
+        stars: 0
+      });
+    }
+});
