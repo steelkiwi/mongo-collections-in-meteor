44323aadec6dc81c93e73a743a6db9be13ade283
uzumaxy <uzumaxy@gmail.com>
2016-07-31 23:56:34 +0300

    Identation.

--- collections-test/server/fill-db.js
+++ collections-test/server/fill-db.js
@@ -5,17 +5,17 @@ Meteor.startup(() => {
     const count = ProgrammingLanguages.find().count();
 
     if (count === 0) {
-      ProgrammingLanguages.insert({
-        name: 'JavaScript',
-        stars: 0
-      });
-      ProgrammingLanguages.insert({
-        name: 'Python',
-        stars: 0
-      });
-      ProgrammingLanguages.insert({
-        name: 'Ruby',
-        stars: 0
-      });
+        ProgrammingLanguages.insert({
+            name: 'JavaScript',
+            stars: 0
+        });
+        ProgrammingLanguages.insert({
+            name: 'Python',
+            stars: 0
+        });
+        ProgrammingLanguages.insert({
+            name: 'Ruby',
+            stars: 0
+        });
     }
 });
